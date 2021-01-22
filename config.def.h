03c95459bdff388d54fd6e4c672a633e6c7e4d6d
anguaive <angurg98@gmail.com>
2021-01-22 09:57:47 +0100

    Revert staticstatus patch

--- config.def.h
+++ config.def.h
@@ -23,9 +23,6 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
-/* staticstatus */
-static const int statmonval = 0;
-
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
--- dwm.c
+++ dwm.c
@@ -330,7 +330,7 @@ static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
 static Drw *drw;
-static Monitor *mons, *selmon, *statmon;
+static Monitor *mons, *selmon;
 static Window root, wmcheckwin;
 
 static xcb_connection_t *xcon;
@@ -871,7 +871,7 @@ drawbar(Monitor *m)
     Client *c;
 
     /* draw status first so it can be overdrawn by tags later */
-    if (m == statmon) { /* status is only drawn on user-defined status monitor */
+    if (m == selmon) { /* status is only drawn selected monitor */
         drw_setscheme(drw, scheme[SchemeNorm]);
         tw = TEXTWM(stext); /* 2px right padding */
         drw_text(drw, m->ww - tw, 0, tw, bh, lrpad / 2, stext, 0, True);
@@ -2217,7 +2217,7 @@ updategeom(void)
                 else
                     mons = createmon();
             }
-            for (i = 0, m = mons; i < nn && m; m = m->next, i++) {
+            for (i = 0, m = mons; i < nn && m; m = m->next, i++)
                 if (i >= n
                         || unique[i].x_org != m->mx || unique[i].y_org != m->my
                         || unique[i].width != m->mw || unique[i].height != m->mh)
@@ -2230,9 +2230,6 @@ updategeom(void)
                     m->mh = m->wh = unique[i].height;
                     updatebarpos(m);
                 }
-		if (i == statmonval)
-		    statmon = m;
-	    }
         } else { /* less monitors available nn < n */
             for (i = nn; i < n; i++) {
                 for (m = mons; m && m->next; m = m->next);
@@ -2247,8 +2244,6 @@ updategeom(void)
                 }
 		if (m == selmon)
 		    selmon = mons;
-                if (m == statmon)
-                    statmon = mons;
                 cleanupmon(m);
             }
         }
@@ -2336,7 +2331,7 @@ updatestatus(void)
 {
     if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
         strcpy(stext, "dwm");
-    drawbar(statmon);
+    drawbar(selmon);
 }
 
 void
